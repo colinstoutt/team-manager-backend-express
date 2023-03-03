07d0f93c6de19a778dd372c37a056669b70066f0
cps97 <colinstoutt@gmail.com>
2023-03-02 20:56:29 -0800

    modify user model

--- models/User.js
+++ models/User.js
@@ -2,7 +2,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const SALT_ROUNDS = 6;
-
 const userSchema = new mongoose.Schema(
   {
     name: String,
@@ -18,7 +17,6 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
-
 userSchema.set("toJSON", {
   transform: function (doc, ret) {
     // remove the password property when serializing doc to JSON
